8edf0158a2e109f5d544f8592bc7c7bc93131cab
Pedro Yamacita <pedro.teidi@gmail.com>
2026-03-23 17:06:16 -0300

    add/ search

--- Trabalho1.asm
+++ Trabalho1.asm
@@ -6,6 +6,8 @@ first_wagon: #primeiro vagao locomotiva (12 bits)
 	.space 3 #padding para alinhar o procimo campo
 	.word 0 #ponteiro para o proximo vagao (0 = NULL) - offset 8
 	
+not_found: .asciz "\nVagao nao encontrado no trem.\n"
+
 	.text
 	.align 2
 	.globl main
@@ -16,7 +18,7 @@ main:
 menu_options: #loop para o usuario escolher os comandos
 	addi a7, zero, 5 #le o input para selecionar o comando
 	ecall
-	add t1, zero, a0 #comando selecionado(t2)
+	add t1, zero, a0 #comando selecionado(t1)
 	
 	#switch de comandos
 	addi t0, zero, 1
@@ -113,9 +115,47 @@ list_loop:
     
     	lw t1, 8(t1) # recebe o endereço do proximo vagao (offset 8)
     	j list_loop
-    	
 
+search_wagon:
+	add1 a7, zero, 5
 	ecall
+	add t1, zero, a0 # Guaarda o ID que vamos procurar
+	
+	add t2, zero, s0 # Come�ar do primeiro vagao
+	
+search_loop:
+	beq t2, zero, search_not_found # Pular para caso erro
+	
+	lw t3, 0(t2) # t3 recebe o ID do vagao atual
+	beq t1, t3, search_found # Se o ID em t1 e t3 � igual, achamos
+	
+	lw t2, 8(t2) # t3 recebe o proximo vagao
+	j search_loop
+	
+search_found:
+	lw a0, 0(t2)
+	addi a7, zero, 1
+	ecall
+	
+	addi a0, zero, 32       # Espa�o
+    	addi a7, zero, 11
+    	ecall
+	
+	lbu a0, 4(t0)           # Tipo
+    	addi a7, zero, 11
+    	ecall
+    	
+    	addi a0, zero, 10       # Nova linha
+    	addi a7, zero, 11
+    	ecall
+    	j menu_options
+    
+search_not_found:
+	la a0, not_found
+	addi a7, zero, 4        # Syscall 4: Imprimir String
+    	ecall
+    	j menu_options
+	
 exit:
 	# encerra o programa
 	addi a7, zero, 10
